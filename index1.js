4652fab98668460d16f1044ed09d0625702d8829
Sumansourav Barik <146023959+sumansourav505@users.noreply.github.com>
2024-09-28 01:49:49 +0530

    id selector

---
+++ index1.js
@@ -0,0 +1,6 @@
+document.getElementById("main-heading").textContent = "Fruit World";
+document.getElementById("main-heading").style.color = "orange";
+document.getElementById("header").style.backgroundColor = "green";
+document.getElementById("header").style.borderBottom = "3px solid orange";
+document.getElementById("basket-heading").style.color = "green";
+document.getElementById("thanks").innerHTML="<p>Please visit us again</p>"
